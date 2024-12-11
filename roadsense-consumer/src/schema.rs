5ac0c3709a8ee40495543444374068fe053dba19
Luca Di Bello <lucadibello@proton.me>
2024-12-11 22:54:11 +0100

    Updated schema: now using Geography rather than Geometry (we will use lat-long)

--- roadsense-consumer/src/schema.rs
+++ roadsense-consumer/src/schema.rs
@@ -2,24 +2,26 @@
 
 pub mod sql_types {
     #[derive(diesel::query_builder::QueryId, Clone, diesel::sql_types::SqlType)]
-    #[diesel(postgres_type(name = "geometry"))]
-    pub struct Geometry;
+    #[diesel(postgres_type(name = "geography"))]
+    pub struct Geography;
 }
 
 diesel::table! {
     use diesel::sql_types::*;
-    use super::sql_types::Geometry;
+    use super::sql_types::Geography;
 
     bump_records (id, created_at) {
         id -> Int4,
         device_id -> Text,
         created_at -> Timestamptz,
         bumpiness_factor -> Int2,
-        location -> Geometry,
+        location -> Geography,
     }
 }
 
 diesel::table! {
+    use diesel::sql_types::*;
+
     spatial_ref_sys (srid) {
         srid -> Int4,
         #[max_length = 256]
