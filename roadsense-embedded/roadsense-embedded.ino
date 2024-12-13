0a3ceca997fba38836b0021156265f065003aeae
paolodeidda <paolodeidda.97@gmail.com>
2024-12-13 11:22:25 +0100

    watchdog dummy implementation

--- roadsense-embedded/roadsense-embedded.ino
+++ roadsense-embedded/roadsense-embedded.ino
@@ -7,6 +7,7 @@ using namespace rtos;
 #define TIME_T1 0.5
 #define TIME_T2 1.0
 #define BUFFER_SIZE 10
+#define WATCHDOG_TIMEOUT 3.0  // Watchdog timeout in seconds
 
 // Event queue for task offloading
 events::EventQueue evq;
@@ -20,10 +21,8 @@ Thread t3;
 mbed::Ticker task1;
 mbed::Ticker task2;
 
-// Mutex for synchronization
 Mutex buffer_mutex;
 
-// Queue and memory pool for inter-task communication
 typedef struct {
     int value;  // Example task data
 } message_t;
@@ -69,7 +68,6 @@ private:
     bool full;
 };
 
-// Task queues and memory pools
 Queue<message_t, 10> task1_queue;
 Queue<message_t, 10> task2_queue;
 
@@ -79,24 +77,30 @@ MemoryPool<message_t, 10> task2_pool;
 CircularBuffer task1_buffer;
 CircularBuffer task2_buffer;
 
+Watchdog &watchdog = Watchdog::get_instance();
+
 // Task 1: Example processing function
 void task1_function() {
     message_t *message = task1_pool.alloc();
     if (message) {
-        message->value = rand() % 100;  // Simulate task data
+        message->value = rand() % 100; // simulate task 
         task1_buffer.put(*message);
         task1_pool.free(message);
     }
+
+    watchdog.kick(); // refresh the watchdog timer
 }
 
 // Task 2: Example processing function
 void task2_function() {
     message_t *message = task2_pool.alloc();
     if (message) {
-        message->value = rand() % 50;  // Simulate task data
+        message->value = rand() % 50;  // simulate task
         task2_buffer.put(*message);
         task2_pool.free(message);
     }
+
+    watchdog.kick(); // refresh the watchdog timer
 }
 
 // ISR for Ticker 1
@@ -109,7 +113,6 @@ void task2_isr() {
     evq.call(task2_function);
 }
 
-// Print function to handle data from queues
 void print_results() {
     while (true) {
         message_t msg;
@@ -120,14 +123,18 @@ void print_results() {
         if (task2_buffer.get(msg)) {
             printf("Task2 data: %d\n", msg.value);
         }
-        ThisThread::sleep_for(500ms); // Prevent busy loop
+
+        watchdog.kick(); // refresh the watchdog timer
+        ThisThread::sleep_for(500ms); // no busy loop
     }
 }
 
 void setup() {
+    watchdog.start(WATCHDOG_TIMEOUT * 1000);  // timeout
+
     // Start the periodic tasks
-    task1.attach(&task1_isr, TIME_T1);  // Start ticker for Task 1
-    task2.attach(&task2_isr, TIME_T2);  // Start ticker for Task 2
+    task1.attach(&task1_isr, TIME_T1); 
+    task2.attach(&task2_isr, TIME_T2); 
 
     // Start threads
     t1.start(callback(&evq, &events::EventQueue::dispatch_forever));
@@ -136,6 +143,5 @@ void setup() {
 }
 
 void loop() {
-    // Empty loop; tasks are handled via threads and tickers
-    // TODO: implement watchdog timer
+
 }
